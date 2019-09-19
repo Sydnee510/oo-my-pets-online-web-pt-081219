564fb76fafa54219f3cb7db1a1d241f21e17ef0c
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-09-19 03:53:22 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -51,6 +51,7 @@ class Owner
     Cat.all.select do |cat|
     cat.mood = "nervous"
   end
+  dog.clear
   end 
   def list_pets
     "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
